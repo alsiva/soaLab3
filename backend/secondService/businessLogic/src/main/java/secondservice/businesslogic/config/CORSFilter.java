19f86da414f3a080e45fc23e252a298c98eb0be8
yaroslav <yacheboksarov768@gmail.com>
2025-03-10 11:51:59 +0300

    CORS fixed!

--- backend/secondService/businessLogic/src/main/java/secondservice/businesslogic/config/CORSFilter.java
+++ backend/secondService/businessLogic/src/main/java/secondservice/businesslogic/config/CORSFilter.java
@@ -12,7 +12,6 @@ public class CORSFilter implements ContainerResponseFilter {
 
     @Override
     public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext cres) throws IOException {
-        cres.getHeaders().add("Access-Control-Allow-Origin", "*");
         cres.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
         cres.getHeaders().add("Access-Control-Allow-Credentials", "true");
         cres.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
